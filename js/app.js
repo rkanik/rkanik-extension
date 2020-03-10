45d5ce82f501690c5e5d0ee5aecd3072b92ac5e4
Md Rasel Khandkar <Rasel15-7040@diu.edu.bd>
2020-03-11 03:00:32 +0600

    Project initialized

---
+++ js/app.js
@@ -0,0 +1,5 @@
+
+if (location.host.includes("spotify")) {
+   // Hide spotify AD container
+   document.querySelector("._3c08e30e0b5018cc15edf990e21d3dc5-scss").style.display = 'none'
+}
---
+++ js/facebook.js
@@ -0,0 +1,15 @@
+
+if (location.host.includes("facebook")) {
+
+  // document.body.style.color = 'white'
+
+   // Changing top bluebar color
+   //let bluebarRoot = document.getElementById("bluebarRoot")
+   //bluebarRoot.firstChild.style.backgroundColor = "#212121"
+
+
+   // let mainBgEl = document.querySelector('div')
+   // mainBgEl.style.backgroundColor = '#202020'
+
+   // document.getElementById("contentCol").style.backgroundColor = "#202020"
+}
