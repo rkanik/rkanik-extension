0fd1720a25ece362ad8dd08549428f789f1455a6
Md Rasel Khandkar <Rasel15-7040@diu.edu.bd>
2020-03-12 08:55:17 +0600

    Working on google

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
 
+const hosts = ["spotify", "google", "facebook", "github"]
+let currentHost = location.host
+
+hosts.forEach(host => {
+   if (currentHost.includes(host)) document.body.id = "rkanik-" + host
+})
+
+// if (currentHost.includes('google')) {
+//    let headerTab = document.getElementById('hdtbSum')
+//    headerTab.style.background = "#292a2d"
+// }
+
 if (location.host.includes("spotify")) {
-   // Hide spotify AD container
    document.querySelector("._3c08e30e0b5018cc15edf990e21d3dc5-scss").style.display = 'none'
 }
